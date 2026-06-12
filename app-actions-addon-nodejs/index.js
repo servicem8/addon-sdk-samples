b08c4eb162efd98072bccd1856fdda28ecc0cd14
Ben Ford <ben.ford@servicem8.com>
2026-06-12 15:38:29 +0930

    Modernize app actions sample for Node 24

--- app-actions-addon-nodejs/index.js
+++ app-actions-addon-nodejs/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event) => {
     
     console.log('Received event:', JSON.stringify(event, null, 2));
 
@@ -27,8 +27,8 @@ exports.handler = (event, context, callback) => {
 </html>`;
     
 	//Return Response
-    callback(null, { 
+    return {
 		eventResponse: strHTMLResponse
-	});
+	};
     
 };
